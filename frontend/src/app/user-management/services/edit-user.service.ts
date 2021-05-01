1d3430be0e16ac414009a821d357446e7097e5b1
Erick <eefeaga2000@gmail.com>
2021-04-30 22:38:54 -0300

    Criação do serviço de editar usuário com os metodos de put e delete

---
+++ frontend/src/app/user-management/services/edit-user.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UserModel } from 'src/app/models/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EditUserService {
+
+  constructor(
+    private readonly http: HttpClient,
+  ) { }
+
+
+  /**
+   * Chama a rota para alterar os dados do usuário
+   * @param id Id do usuário
+   * @param data dados para realizar a alteração
+   * @returns Resposta da requisição
+   */
+  updateUserData(id: number, data: UserModel): Observable<any> {
+    return this.http.put(`http://localhost:8080/user/id=${id}`, data);
+  }
+
+  /**
+   * Chama a rota para deletar o usuário
+   * @param id Id do usuário
+   * @returns Resposta da requisição
+   */
+  deleteUser(id: number): Observable<any> {
+    return this.http.delete(`http://localhost:8080/user/${id}`);
+  }
+}
